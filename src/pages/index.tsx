e5c2c230931551944e1f7f6377584c809e6d1351
Igor <nascimentoigor49@gmail.com>
2023-02-06 00:55:59 -0300

    Terminando de alterar o cabecalho fixo da pagina inicial.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,26 +5,33 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import styles from "./index.module.css";
+import Background from "@site/src/components/Background";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
+      <header className={clsx("hero hero--black", styles.heroBanner)}>
+        <Background/>
+      </header>
   );
 }
+
+/*
+<header className={clsx("hero hero--primary", styles.heroBanner)}>
+  <div className="container">
+    <h1 className="hero__title">{siteConfig.title}</h1>
+    <p className="hero__subtitle">{siteConfig.tagline}</p>
+    <div className={styles.buttons}>
+      <Link
+          className="button button--secondary button--lg"
+          to="/docs/intro"
+      >
+        Docusaurus Tutorial - 5min ⏱️
+      </Link>
+    </div>
+  </div>
+</header>
+*/
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
